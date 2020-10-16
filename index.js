239affc08ae9cb6fa574753969aaf60c72c8b696
Michael Bernasol <mfbernasol@gmail.com>
2020-10-15 20:56:09 -0500

    console logging zoom and bounds, need to add checkcoords function

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
  
-let map;
+var favPlaces = [
+{
+  content: 'Los Angleles',
+  coords:{lat:34.052235,lng: -118.243683}
+},
+{
+  content: 'Chicago',
+  coords:{lat: 41.878113,lng: -87.629799}
+}
+
+];
+var locIndex = 0;
+var currentLoc = favPlaces[locIndex];
+
 
 function initMap() {
     
-const myLatLng = {lat: 41.878113,lng: -87.629799};
-  map = new google.maps.Map(document.getElementById("map"), {
+  const myLatLng = {lat: 41.878113,lng: -87.629799};
+
+   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 41.878113, lng: -87.629799},
-    zoom: 5,
+    zoom: 3,
   });
+
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+		console.log(map.getBounds());
+	});
+
+  //listens to clicks to add markers
   map.addListener("click", function(e) {
     addMarker(e.latLng);
     console.log(e);
   });
 
 
+  map.addListener('zoom_changed',function(){
+    console.log("zoom level: " + map.getZoom());
+  });
 
 }
 
+//initializes markers
 function addMarker(latLng) {
   var marker = new google.maps.Marker({
       position: latLng,
@@ -35,3 +59,20 @@ function addMarker(latLng) {
   });
 
 }
+
+var locIndex = 0;
+var currentLoc = favPlaces[locIndex];
+
+
+//Checks array for favorite place 
+function checkPlace() {
+  
+
+}
+
+
+//Count Sore
+
+
+
+//Give hints
