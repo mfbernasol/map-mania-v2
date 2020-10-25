355c7fcd0661c10e7a8dfed7c1c495b2ec2ad24f
Michael Bernasol <mfbernasol@gmail.com>
2020-10-25 15:23:56 -0500

    add another alert

--- index.js
+++ index.js
@@ -58,9 +58,8 @@ var locIndex = 0;
 var currentLoc = favPlaces[locIndex];
 
 function initMap() {
-  window.confirm("Getting Started: To play follow the hints and zoom in the location to score points");
+  window.confirm("Getting Started: To play follow the hints and zoom in the location to score points. Or just click the button to win!");
   window.confirm("Your first hint: Hollywood is here");
-  window.confirm("Click the button if you wanna just win!");
   const myLatLng = { lat: 41.878113, lng: -87.629799 };
 
   map = new google.maps.Map(document.getElementById("map"), {
